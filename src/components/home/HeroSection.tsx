842181488b19b80d093819630b8bc1099f9047c1
labibhasan173 <139766964+labibhasan173@users.noreply.github.com>
2025-07-25 16:47:33 +0600

    a

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -84,8 +84,8 @@ const HeroSection = () => {
             className="w-32 h-32 sm:w-40 sm:h-40 mx-auto rounded-full bg-tech-gradient p-1 shadow-[0_0_30px] shadow-primary/50"
           >
             <img 
-              src="/lovable-uploads/mypic.png"
-              alt="Md.Labib Hasan" 
+              src="/uploads/mypic.png"
+               
               className="w-full h-full rounded-full object-cover border-2 border-background"
             />
           </motion.div>
